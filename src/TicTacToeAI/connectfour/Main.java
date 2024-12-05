49ceda4f0d4f13de5ec221a1106df958f1a54807
nadialvy <5026231007@student.its.ac.id>
2024-12-05 12:39:48 +0700

    feat: sudoku

---
+++ src/TicTacToeAI/connectfour/Main.java
@@ -0,0 +1,10 @@
+package TicTacToeAI.connectfour;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Connect Four");
+    ConnectFour game = new ConnectFour();
+    game.play();
+
+  }
+}
