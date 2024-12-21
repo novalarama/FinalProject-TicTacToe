e95fc3365cff4f1cb37007e2e87b47c4a02d9aa7
nadialvy <5026231007@student.its.ac.id>
2024-12-21 16:39:22 +0700

    feat: rebasing

--- src/GUI/RoundedTextFieldUI.java
+++ src/GUI/RoundedTextFieldUI.java
@@ -4,12 +4,12 @@ import java.awt.*;
 import javax.swing.plaf.basic.BasicTextFieldUI;
 
 public class RoundedTextFieldUI extends BasicTextFieldUI {
-    @Override
-    protected void paintSafely(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g.create();
-        g2d.setColor(new Color(30, 30, 30));
-        g2d.fillRoundRect(0, 0, getComponent().getWidth(), getComponent().getHeight(), 40, 40);
-        g2d.dispose();
-        super.paintSafely(g);
-    }
+  @Override
+  protected void paintSafely(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+    g2d.setColor(new Color(30, 30, 30));
+    g2d.fillRoundRect(0, 0, getComponent().getWidth(), getComponent().getHeight(), 40, 40);
+    g2d.dispose();
+    super.paintSafely(g);
+  }
 }
